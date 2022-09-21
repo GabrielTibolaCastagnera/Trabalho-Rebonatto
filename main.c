6200d104a21a89cb0afbac87ee6d47383c85aaf9
GabrielTibolaCastagnera <gabriel.tibola.castagnera@gmail.com>
2022-09-21 14:33:12 -0300

    main code

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+Gabriel Tibola Castagnera
+Willian Brun
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h> 
+#include <sys/resource.h>
+#include <fcntl.h>
+int main(int argc, char *argv[]){
+    return 0;
+}
