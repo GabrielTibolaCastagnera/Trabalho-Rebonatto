d9ae4872aa1469bed8f6f4ca6ff7bea8606d3979
GabrielTibolaCastagnera <gabriel.tibola.castagnera@gmail.com>
2022-09-24 22:53:06 -0300

    childs finished

--- main.c
+++ main.c
@@ -17,9 +17,60 @@ Willian Brun
 #include <sys/sysinfo.h>
 #include <sys/wait.h>
 #include <sys/prctl.h>
+#include <errno.h>
+#include <math.h>
+
+void filho1(int num)
+{
+    int divisor = 1;
+    int add = 1;
+    if (num % 2)
+    {
+        add++;
+    }
+    float max = sqrt(num);
+    long long sum = 0;
+    for (divisor; divisor <= max; divisor += add)
+    {
+        if (num % divisor == 0)
+        {
+            sum += divisor + num / divisor;
+        }
+    }
+    printf("Soma dos números divisores de %d: %lld\n", num, sum);
+    return;
+}
+void filho2(int num)
+{
+    if (num % 2 == 0 || num == 1)
+    {
+        printf("O número %d não é primo!\n", num);
+        return;
+    }
+    int divisor = 3;
+    float max = sqrt(num);
+    for (divisor; divisor <= max; divisor += 2)
+    {
+        if (num % divisor == 0)
+        {
+            printf("O número %d não é primo!\n", num);
+            return;
+        }
+    }
+    printf("O número %d é primo!\n", num);
+    return;
+}
+void filho3()
+{
+    int waitfor = 1 + rand() % 7;
+    printf("Filho 3 Aguardando %d segundos\n", waitfor);
+    sleep(waitfor);
+    return;
+}
 
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     // Verifica se apenas/exatamente dois parâmetros foram informados
     if (argc < 3)
     {
@@ -31,7 +82,8 @@ int main(int argc, char *argv[])
         printf("Too many arguments\n");
         exit(1);
     }
-
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
     // //Verifica se os parâmetros contém pelo menos 10 digitos
     // if()
     // {
@@ -44,7 +96,7 @@ int main(int argc, char *argv[])
     pidAtual = getpid();
 
     // TODO:Máximo de processos concorrentes
-    long numOfProcess;
+    long numOfProcess = sysconf(_SC_CHILD_MAX);
 
     // Get sysinfo
     struct sysinfo info;
@@ -68,74 +120,71 @@ int main(int argc, char *argv[])
     printf("Processo [%d]. Pai tem como pai o processo: %d\n", pidAtual, parentPid);
     printf("Processo [%d]. Processos em execução: %ld\n", pidAtual, sysconf(_SC_CHILD_MAX));
 
+    //Armazena os pids dos filhos
     pid_t children[3];
     pid_t pid;
     time_t t;
     int status;
-
+    //Cria o primeiro Filho
     if ((children[0] = fork()) < 0)
         perror("fork() error");
     else if (children[0] == 0)
     {
-        prctl(PR_SET_PDEATHSIG, SIGTERM);
-        time(&t);
-        printf("child (pid %d) started at %s", (int)getpid(), ctime(&t));
-        time(&t);
-        printf("child exiting at %s", ctime(&t));
-        exit(1);
+        //Filho 1
+        printf("Processo [%d]. Filho 1 tem como pai o processo: %d.\n", getpid(), getppid());
+        filho1(num1);
+        printf("Processo [%d]. Filho 1 vai terminar sua execução ...\n", getpid());
     }
     else
     {
+        //Cria o segundo Filho
         if ((children[1] = fork()) < 0)
             perror("fork() error");
         else if (children[1] == 0)
         {
-            prctl(PR_SET_PDEATHSIG, SIGTERM);
-            time(&t);
-            printf("child (pid %d) started at %s", (int)getpid(), ctime(&t));
-            time(&t);
-            printf("child exiting at %s", ctime(&t));
-            exit(2);
+            //Filho 2
+            printf("Processo [%d]. Filho 2 tem como pai o processo: %d.\n", getpid(), getppid());
+            filho2(num2);
+            printf("Processo [%d]. Filho 2 vai terminar sua execução ...\n", getpid());
         }
         else
         {
+            //Cria o terceiro filho
             if ((children[2] = fork()) < 0)
                 perror("fork() error");
             else if (children[2] == 0)
             {
-                prctl(PR_SET_PDEATHSIG, SIGTERM);
-                time(&t);
-                printf("child (pid %d) started at %s", (int)getpid(), ctime(&t));
-                time(&t);
-                printf("child exiting at %s", ctime(&t));
-                exit(3);
+                //Filho 3
+                printf("Processo [%d]. Filho 3 tem como pai o processo: %d.\n", (int)getpid(), getppid());
+                filho3();
+                printf("Processo [%d]. Filho 3 apto novamente. Vai terminar sua execução. Fim do Filho3\n", getpid());
             }
             else
             {
+                //Espera um dos Filhos Terminarem e retorna o seu pid
+                pid = wait(&status);
+                system("ps");
+                //Encerra todos os filhos
+                for (int i = 0; i < 3; i++)
+                {
+                    if (kill(children[i], SIGTERM) == -1 && errno != ESRCH)
+                    {
+                        /*Free resources*/
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                int filho;
+                //Procura qual filho Terminou
+                for(int i = 0; i < 3; i++)
+                {
+                    if (children[i] == pid)
+                        filho = i + 1;
+                }
+                printf("O filho %d com pid %d terminou primeiro\n", filho, pid);
+
+                time(&t);
             }
         }
     }
-    printf("parent has forked child with pid of %d\n", (int)pid);
-    time(&t);
-    printf("parent is starting wait at %s", ctime(&t));
-    if ((pid = wait(&status)) == -1)
-        perror("wait() error");
-    else
-    {
-        time(&t);
-        printf("parent is done waiting at %s", ctime(&t));
-        printf("the pid of the process that ended was %d\n", (int)pid);
-
-        if (WIFEXITED(status))
-            printf("child exited with status of %d\n", WEXITSTATUS(status));
-        else if (WIFSIGNALED(status))
-            printf("child was terminated by signal %d\n",
-                   WTERMSIG(status));
-        else if (WIFSTOPPED(status))
-            printf("child was stopped by signal %d\n", WSTOPSIG(status));
-        else
-            puts("reason unknown for child termination");
-    }
-
-    return 0;
+    exit(0);
 }
